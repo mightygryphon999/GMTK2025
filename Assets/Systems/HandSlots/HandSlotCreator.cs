e87dda605fcb08081e10a5894c662b0dd886979b
mightygryphon999 <griffin.roberts.srq@gmail.com>
2025-08-01 19:22:48 -0400

    Made it so that it switched table tezxtures

--- Assets/Systems/HandSlots/HandSlotCreator.cs
+++ Assets/Systems/HandSlots/HandSlotCreator.cs
@@ -5,10 +5,15 @@ public class HandSlotCreator : MonoBehaviour
 {
     public GameController gc;
     public List<GameObject> slots;
+    public List<Texture> textures;
+    public Renderer rend;
+    public int shownSlots;
     public void createNewHandSlot()
     {
         if (slots.Count != 0)
         {
+            shownSlots++;
+            rend.material.mainTexture = textures[shownSlots - 1];
             GameObject newSlot = slots[0];
             slots[0].SetActive(true);
             gc.hand.Add(slots[0].GetComponent<CardPlacement>());
